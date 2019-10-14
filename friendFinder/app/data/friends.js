b3c18cb98bf070e2ec41cecda8d57c198dc7d630
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-14 12:46:48 -0600

    debugged images

--- friendFinder/app/data/friends.js
+++ friendFinder/app/data/friends.js
@@ -1,117 +1,117 @@
-var friends = 
-[{
-    name:"Shredder",
-    photo:"https://images.app.goo.gl/Wgrh1cCAeNheN5Qw8",
-    scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+var friends =
+  [{
+    name: "Shredder",
+    photo: "https://img.fireden.net/co/image/1518/54/1518545390440.png",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
   },
   {
-    name:"Splinter",
-    photo:"https://images.app.goo.gl/v4DUNehHgZxwYqzL6",
-    scores:[
-        4,
-        3,
-        2,
-        1,
-        5,
-        1,
-        2,
-        3,
-        4,
-        1
-      ]
+    name: "Splinter",
+    photo: "http://files.gamebanana.com/img/ico/sprays/splinter.png",
+    scores: [
+      4,
+      3,
+      2,
+      1,
+      5,
+      1,
+      2,
+      3,
+      4,
+      1
+    ]
   },
   {
-    name:"April",
-    photo:"https://images.app.goo.gl/WHBRj8pqjpPZhDuKA",
-    scores:[
-        1,
-        2,
-        4,
-        3,
-        4,
-        1,
-        1,
-        3,
-        1,
-        1
-      ]
+    name: "April",
+    photo: "https://pbs.twimg.com/profile_images/3330357494/a451f3fd59d71aafd4060d1d4e2d02bc.jpeg",
+    scores: [
+      1,
+      2,
+      4,
+      3,
+      4,
+      1,
+      1,
+      3,
+      1,
+      1
+    ]
   },
   {
-    name:"Leonardo",
-    photo:"https://images.app.goo.gl/xkwNNDJzVAfwjLd66",
-    scores:[
-        1,
-        2,
-        1,
-        3,
-        4,
-        4,
-        3,
-        1,
-        1,
-        1
-      ]
+    name: "Leonardo",
+    photo: "https://i.pinimg.com/originals/ce/31/93/ce31937db0330dd15a619ca47b5176b9.png",
+    scores: [
+      1,
+      2,
+      1,
+      3,
+      4,
+      4,
+      3,
+      1,
+      1,
+      1
+    ]
   },
   {
-    name:"Rapheal",
-    photo:"https://images.app.goo.gl/RnbR96E46JwjnLS16",
-    scores:[
-        1,
-        2,
-        4,
-        5,
-        4,
-        4,
-        3,
-        2,
-        2,
-        1
-      ]
+    name: "Rapheal",
+    photo: "https://i.pinimg.com/originals/2c/cd/17/2ccd1759c36d6474b4be47d17263018d.png",
+    scores: [
+      1,
+      2,
+      4,
+      5,
+      4,
+      4,
+      3,
+      2,
+      2,
+      1
+    ]
   },
   {
-    name:"Michelangelo",
-    photo:"https://images.app.goo.gl/yPuqk7vBhk1g8Ni97",
-    scores:[
-        1,
-        2,
-        4,
-        3,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-      ]
+    name: "Michelangelo",
+    photo: "https://i.pinimg.com/474x/ff/3d/a3/ff3da37403c051225f7d2107a1578795--ninja-birthday-turtle-birthday.jpg",
+    scores: [
+      1,
+      2,
+      4,
+      3,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
   },
   {
-    name:"Donatello",
-    photo:"https://images.app.goo.gl/QW6Zyt6J7WaqZvaE7",
-    scores:[
-        1,
-        2,
-        4,
-        3,
-        4,
-        4,
-        1,
-        3,
-        1,
-        1
-      ]
+    name: "Donatello",
+    photo: "https://www.80stoystore.com/wp-content/uploads/2019/09/donatello.png",
+    scores: [
+      1,
+      2,
+      4,
+      3,
+      4,
+      4,
+      1,
+      3,
+      1,
+      1
+    ]
   }
 
-];
+  ];
 
 module.exports = friends;
